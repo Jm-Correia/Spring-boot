ac9ece8b480e6c3c2a8bc01705df04ddb35d9d0c
Joao Correia <joao.correia@pitang.com>
2019-09-01 17:06:00 -0300

    First Commit

---
+++ src/main/java/br/com/c2c/condosdecision/CondosDecisionApplication.java
@@ -0,0 +1,13 @@
+package br.com.c2c.condosdecision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondosDecisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondosDecisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/c2c/condosdecision/CondosDecisionApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.c2c.condosdecision;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CondosDecisionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
