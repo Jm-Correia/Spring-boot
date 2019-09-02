48ee41ad9508098ecd99124196b828500315ed04
Joao Correia <joao.correia@pitang.com>
2019-09-02 20:39:48 -0300

    Adicionado Banco de dados H2 de teste e criação das tables

--- src/main/java/br/com/c2c/condosdecision/domain/Categoria.java
+++ src/main/java/br/com/c2c/condosdecision/domain/Categoria.java
@@ -2,11 +2,24 @@ package br.com.c2c.condosdecision.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Categoria")
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-		
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(name = "Nome", length = 50, nullable = false)
 	private String nome;
 		
 	public Categoria() {
