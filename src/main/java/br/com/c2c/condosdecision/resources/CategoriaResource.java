8fe5171b88b483fa0a58a8c31d5f20587629888d
Joao Correia <joao.correia@pitang.com>
2019-09-02 20:12:00 -0300

    Testes retorno lista no Rest

--- src/main/java/br/com/c2c/condosdecision/resources/CategoriaResource.java
+++ src/main/java/br/com/c2c/condosdecision/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package br.com.c2c.condosdecision.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.c2c.condosdecision.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String listar() {
-		return "Rest Está Funcionando!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria>  categorias = new ArrayList<>();
+		categorias.add(cat1);
+		categorias.add(cat2);
+		
+		return categorias;
 	}
 	
 	
